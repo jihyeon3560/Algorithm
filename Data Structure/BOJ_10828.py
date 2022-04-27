58b6934770fa310f06af57056d4cc781732fdfd7
jihyeon3560 <jihyeon3560@gmail.com>
2022-04-28 00:15:43 +0900

    스택

---
+++ Data Structure/BOJ_10828.py
@@ -0,0 +1,28 @@
+import sys
+
+N = int(sys.stdin.readline())
+stack = []
+
+for _ in range(N):
+    tmp = sys.stdin.readline().split()
+
+    if tmp[0] == 'push':
+        stack.append(tmp[-1])
+    elif tmp[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif tmp[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+    elif tmp[0] == 'size':
+        print(len(stack))
+
+    elif tmp[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
