97354b068ed564cde168efe0abca4879263caddd
jihyeon3560 <jihyeon3560@gmail.com>
2022-04-27 23:35:29 +0900

    정수 제곱근

--- binary_search/BoJ_2417.py
+++ binary_search/BoJ_2417.py
@@ -4,11 +4,12 @@ left = 0
 right = n
 
 while left <= right:
-    mid = (left + right)//2
 
+    mid = (left + right)//2
     if mid**2 < n:
         left = mid + 1
     else:
         right = mid - 1
+
 
 print(left)
